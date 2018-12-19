9f783046ddc7dcc3d62048ab2f033330a7b17415
meaning <1491436783@qq.com>
2018-12-19 11:10:52 +0800

    11 Signed-off-by: meaning <1491436783@qq.com>

---
+++ src/domain/ThemeBean.java
@@ -0,0 +1,49 @@
+package domain;
+
+public class ThemeBean {
+
+	int id;
+	String title;
+	String contents;
+	String classity;
+	int sender;
+	String date;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getClassity() {
+		return classity;
+	}
+	public void setClassity(String classity) {
+		this.classity = classity;
+	}
+	public int getSender() {
+		return sender;
+	}
+	public void setSender(int sender) {
+		this.sender = sender;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+}
