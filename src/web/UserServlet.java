13f87e6d2b678aca35077382a05d1b5ff15b147b
Ash <Glaxyinfinite@outlook.com>
2018-12-19 10:56:59 +0800

    Signed-off-by: Ash <Glaxyinfinite@outlook.com>

--- src/web/UserServlet.java
+++ src/web/UserServlet.java
@@ -2,13 +2,13 @@ package web;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import dao.UserDao;
 import domain.UserBean;
 
+//  ÓÃ»§Servlet
+@SuppressWarnings("serial")
 public class UserServlet extends BaseServlet {
 	
 	//  µÇÂ¼
