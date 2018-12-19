43f942357c6d63efe8c0897b49656dda89870cd2
meaning <1491436783@qq.com>
2018-12-19 10:55:06 +0800

    封装好了 Signed-off-by: meaning <1491436783@qq.com>

--- src/domain/UserBean.java
+++ src/domain/UserBean.java
@@ -4,9 +4,164 @@ public class UserBean {
 	int id;
 	String username;
 	String password;
+	String face;
+	int rank;
+	int points;
+	int warm_count;
+	int random_count;
+	int virtual_count;
+	int weekly_count;
+	int warm_win;
+	int random_win;
+	int virtual_win;
+	int weekly_win;
+	String question;
+	String code;
+	String title;
+	String contents;
+	String classify;
+	int sender;
+	String date;
+	int theme;
+	int sequence;
+	String image;
+	String video;
+	
 	
 	public int getId() {
 		return id;
+	}
+	public String getFace() {
+		return face;
+	}
+	public void setFace(String face) {
+		this.face = face;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public int getWarm_count() {
+		return warm_count;
+	}
+	public void setWarm_count(int warmCount) {
+		warm_count = warmCount;
+	}
+	public int getRandom_count() {
+		return random_count;
+	}
+	public void setRandom_count(int randomCount) {
+		random_count = randomCount;
+	}
+	public int getVirtual_count() {
+		return virtual_count;
+	}
+	public void setVirtual_count(int virtualCount) {
+		virtual_count = virtualCount;
+	}
+	public int getWeekly_count() {
+		return weekly_count;
+	}
+	public void setWeekly_count(int weeklyCount) {
+		weekly_count = weeklyCount;
+	}
+	public int getWarm_win() {
+		return warm_win;
+	}
+	public void setWarm_win(int warmWin) {
+		warm_win = warmWin;
+	}
+	public int getRandom_win() {
+		return random_win;
+	}
+	public void setRandom_win(int randomWin) {
+		random_win = randomWin;
+	}
+	public int getVirtual_win() {
+		return virtual_win;
+	}
+	public void setVirtual_win(int virtualWin) {
+		virtual_win = virtualWin;
+	}
+	public int getWeekly_win() {
+		return weekly_win;
+	}
+	public void setWeekly_win(int weeklyWin) {
+		weekly_win = weeklyWin;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getClasss() {
+		return classify;
+	}
+	public void setClasss(String classify) {
+		this.classify = classify;
+	}
+	public int getSender() {
+		return sender;
+	}
+	public void setSender(int sender) {
+		this.sender = sender;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getTheme() {
+		return theme;
+	}
+	public void setTheme(int theme) {
+		this.theme = theme;
+	}
+	public int getSequence() {
+		return sequence;
+	}
+	public void setSequence(int sequence) {
+		this.sequence = sequence;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getVideo() {
+		return video;
+	}
+	public void setVideo(String video) {
+		this.video = video;
 	}
 	public void setId(int id) {
 		this.id = id;
