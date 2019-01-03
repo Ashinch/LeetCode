1b7d6f3d468e3a6bb0fdea60f62b9b1af020ed57
Ash <Glaxyinfinite@outlook.com>
2019-01-03 11:44:36 +0800

    Signed-off-by: Ash <Glaxyinfinite@outlook.com>

--- src/Controllers/RankController.java
+++ src/Controllers/RankController.java
@@ -33,6 +33,7 @@ public class RankController {
 	 * @param 	request		通过链接/rank
 	 * @return 	String		跳转到rank.jsp
 	 **************************************************/
+	@SuppressWarnings("unchecked")
 	@RequestMapping(value="/rank")
 	public String index(HttpServletRequest request) {
 		int currentPage = 1;
