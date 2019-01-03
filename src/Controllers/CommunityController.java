a3fc1b5f0471458c39f3467afdf026e76943c235
Ash <Glaxyinfinite@outlook.com>
2019-01-03 08:12:49 +0800

    Signed-off-by: Ash <Glaxyinfinite@outlook.com>

--- src/Controllers/CommunityController.java
+++ src/Controllers/CommunityController.java
@@ -3,9 +3,24 @@ package Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**************************
+ * 说明： 社区控制器
+ *************************** 
+ * 类名： CommunityController 
+ * 包名： Controllers
+ ***************************/
 @Controller
 public class CommunityController {
-	@RequestMapping(value="/community")
+	
+	/**************************************************
+	 * 限定符：	公开
+	 * 说明：	跳转社区页面
+	 * 方法名：	index
+	 **************************************************
+	 * 参数表：
+	 * @return 	String	跳转到community.jsp
+	 **************************************************/
+	@RequestMapping(value = "/community")
 	public String index() {
 		return "community";
 	}
